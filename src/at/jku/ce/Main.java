8c6e717dbf94e24e50b5444b8fc2820eaca1f976
Pascal Hoefler <pascal.hoefler@gmx.at>
2011-05-13 16:21:34 +0200

    <Tag 5> Clemens: Kommentare in der Main.java hinzugefuegt.

--- src/at/jku/ce/Main.java
+++ src/at/jku/ce/Main.java
@@ -8,11 +8,11 @@ public class Main {
 	
 	Set<DomainObject> objectSet = new HashSet<DomainObject>();
 
-	objectSet.add(new DomainObject("Helga"));
-	objectSet.add(new DomainObject("Walter"));
-	objectSet.add(new DomainObject("Hannah"));
-	objectSet.add(new DomainObject("Paul"));
-	objectSet.add(new DomainObject("Claudia"));
+	objectSet.add(new DomainObject("Helga", "Das ist mein Kommentar."));
+	objectSet.add(new DomainObject("Walter", "Das ist mein Kommentar."));
+	objectSet.add(new DomainObject("Hannah", "Das ist mein Kommentar."));
+	objectSet.add(new DomainObject("Paul", "Das ist mein Kommentar."));
+	objectSet.add(new DomainObject("Claudia", "Das ist mein Kommentar."));
 
 	for (DomainObject domainObject : objectSet) {
 	  LOGGER.debug(domainObject.id);
