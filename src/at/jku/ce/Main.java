69f36a8ffaa2fccb6c941617451b7b804cb9d446
Mario <mario_starlinger@gmx.at>
2011-05-13 16:35:04 +0200

    <Tag6> Barbara: Methodenaufruf auf getUuId() geaendert

--- src/at/jku/ce/Main.java
+++ src/at/jku/ce/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	objectSet.add(new DomainObject("Claudia", "Das ist mein Kommentar."));
 
 	for (DomainObject domainObject : objectSet) {
-	  LOGGER.debug(domainObject.id);
+	  LOGGER.debug(domainObject.getUuid());
 	}
 
 	}
